05659572e3ce893d53509e3523ce08cb51bd044e
Austin <austin.wentling@gmail.com>
2022-05-16 11:13:48 -0600

    Homepage Display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,48 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 import NucampLogo from "../app/assets/img/logo.png";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <Navbar dark color="primary" sticky="top" expand="md">
-      <NavbarBrand href="/">
-        <img src={NucampLogo} alt="nucamp logo" />
+      <NavbarBrand href="/" className="ms-5">
+        <img src={NucampLogo} alt="nucamp logo" className="float-start" />
+        <h1 className="mt-1">NuCamp</h1>
       </NavbarBrand>
+      <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+      <Collapse isOpen={menuOpen} navbar>
+        <Nav className="ms-auto" navbar>
+          <NavItem>
+            <NavLink className="nav-link" to="/">
+              <i class="fa fa-home fa-lg" /> Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/directory">
+              <i class="fa fa-list fa-lg" /> Directory
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/about">
+              <i class="fa fa-info fa-lg" /> About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/contact">
+              <i class="fa fa-info-card fa-lg" /> Contact
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
     </Navbar>
   );
 };
