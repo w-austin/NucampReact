3c7f59ee0f6b93f083772a06206095e3f3ebea16
Austin <austin.wentling@gmail.com>
2022-06-02 22:56:57 -0600

    Async DisplayList

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -47,6 +47,12 @@ export const selectAllPartners = (state) => {
   return state.partners.partnersArray;
 };
 
-export const selectFeaturedPartner = (state) => {
-  return state.partners.partnersArray.find((partner) => partner.featured);
+export const selectFeaturedPartner =  (state) => {
+  return {
+    featuredItem: state.partners.partnersArray.find(
+      (partner) => partner.featured
+    ),
+    isLoading: state.partners.isLoading,
+    errMsg: state.partners.errMsg,
+  };
 };
