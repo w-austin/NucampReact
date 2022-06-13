3ad325c100e55336baa12b6fbf5473f8791e3155
Austin <austin.wentling@gmail.com>
2022-06-13 12:39:49 -0600

    Week 5 React Workshop

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import { fetchCampsites } from "./features/campsites/campsitesSlice";
 import { fetchPartners } from "./features/partners/partnersSlice";
 import { fetchPromotions } from "./features/promotions/promotionsSlice";
+import { fetchComments } from "./features/comments/commentsSlice";
 import "./App.css";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
     dispatch(fetchPromotions());
+    dispatch(fetchComments());
   }, [dispatch]);
 
   return (
--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -11,10 +11,10 @@ import Loading from "../components/Loading";
 const CampsiteDetailPage = () => {
   const { campsiteId } = useParams();
   const campsite = useSelector(selectCampsiteById(campsiteId));
-  console.log("campsite:", campsite);
+  console.log("campsite", campsite);
 
   const isLoading = useSelector((state) => state.campsites.isLoading);
-  const errMsg = useSelector((state) => state.campsites.error);
+  const errMsg = useSelector((state) => state.campsites.errMsg);
   let content = null;
 
   if (isLoading) {
